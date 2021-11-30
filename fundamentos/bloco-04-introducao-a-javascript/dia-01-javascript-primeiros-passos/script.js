562394a5d770d9c26e0632b88d760cc3d1c7528e
Luiz Guilherme Avelino <guilhermerx18@gmail.com>
2021-11-30 19:24:45 -0300

    att2

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,32 @@
+let Porcentagem = 92
+
+switch (Porcentagem) {
+  case >= 90:
+  console.log("A");
+  break;
+
+  case >= 80:
+  console.log("B")
+  break;
+
+  case >= 70:
+  console.log("C")
+  break;
+
+  case >= 60:
+  console.log("D")
+  break;
+
+  case >= 50:
+  console.log("E")
+  break;
+
+  case < 50:
+  console.log("F")
+  break;
+
+  default:
+    console.log("não se aplica")
+
+}
+  
