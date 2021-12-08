0c507871b8866d7253b2fe69e58c957f65e85569
Luiz Guilherme Avelino <guilhermerx18@gmail.com>
2021-12-08 20:05:26 -0300

    exercicio 5.4 concluido

--- fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-04-javascript-web-storage/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-04-javascript-web-storage/script.js
@@ -7,35 +7,46 @@ let inputFont = document.querySelector('#input-font')
 let inputSize = document.querySelector('#input-size')
 let inputEsp = document.querySelector('#input-esp')
 let texto = document.getElementsByClassName('texto')
+let remove = document.getElementById('btn-resetar')
 
 function mudabg(){
     btnadd.addEventListener('click',function(event){
+
+        localStorage.setItem('cachebg',inputbg.value)
         bodycss.style.backgroundColor = inputbg.value
     });
-}
 
+    bodycss.style.backgroundColor = localStorage.getItem('cachebg')
+}
 mudabg()
 
 function colortext(){
     btnadd.addEventListener('click',function(){
+        localStorage.setItem('cachetc',inputColor.value)
         paginas.style.color = inputColor.value
-    })
+    });
+    paginas.style.color = localStorage.cachetc
 }
 
 colortext()
 
 function alteraFonte(){
+    
     btnadd.addEventListener('click',function(){
+        localStorage.setItem('cachefont',inputFont.value)
         bodycss.style.fontFamily = inputFont.value
     });
+    bodycss.style.fontFamily = localStorage.cachefont
 }
 
 alteraFonte()
 
 function tamanhoFont(){
     btnadd.addEventListener('click',function(){
+        localStorage.setItem('cachesize',inputSize.value)
         paginas.style.fontSize = inputSize.value
     });
+    paginas.style.fontSize = localStorage.cachesize
 }
 
 tamanhoFont()
@@ -43,9 +54,17 @@ tamanhoFont()
 
 function espacamento(){
     btnadd.addEventListener('click',function(){
+        localStorage.setItem('cacheesp',inputEsp.value)
         texto[0].style.lineHeight = inputEsp.value
         texto[1].style.lineHeight = inputEsp.value
     });
+
+    texto[0].style.lineHeight = localStorage.cacheesp
+    texto[1].style.lineHeight = localStorage.cacheesp
 }
 
 espacamento()
+
+remove.addEventListener('click',function(){
+    
+})
