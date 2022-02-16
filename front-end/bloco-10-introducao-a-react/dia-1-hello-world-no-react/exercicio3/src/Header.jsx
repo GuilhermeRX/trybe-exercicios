ecb5066a75a200df338a6efaa5c1758b7d2e315d
Luiz Guilherme Avelino <guilhermerx18@gmail.com>
2022-02-16 19:05:48 -0300

    exercicios do dia 1

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio3/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <h1 className="Title">Conteúdos de Front-End</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
